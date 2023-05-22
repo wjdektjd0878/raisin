e30f90697b5144b51c9240534b844e5f6b21e75b
정다성 <wjdektjd0878@gmail.com>
2023-05-22 15:02:51 +0900

    story 코드 내 point 추가 및 수정

--- new_python/main.py
+++ new_python/main.py
@@ -1,6 +1,23 @@
 # main.py
 import random
+import os
 import story
+import logging
+
+logging.basicConfig(filename='./app.log', filemode='w', level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
+
+def example_function(param):
+    logging.info('Starting example_function')
+    try:
+        # Function logic goes here...
+        logging.debug('This is a debug message')
+    except Exception as e:
+        logging.error('An error occurred: ' + str(e))
+    finally:
+        logging.info('Finished example_function')
+
+# Somewhere else in your code...
+example_function("test parameter")
 
 # Player의 Character 생성
 class Character:
@@ -11,7 +28,9 @@ class Character:
 def main():
     name = input('플레이어의 이름을 입력하세요: ')
     player = Character(name)
+    print(story.get_main_story1(name))
     story.progress_story(player)
 
 if __name__ == "__main__":
     main()
+
--- new_python/story.py
+++ new_python/story.py
@@ -1,10 +1,62 @@
 # story.py
+import os
 import random
+
+def get_main_story1(player_name, choice=None):
+    print(f'Chapter 1: 대피')
+    story_path = 'main_story.txt'
+
+    with open(f'{story_path}', 'r', encoding='utf-8') as file:
+        story = file.read()
+
+    return story.replace('{name}', player_name)
 
 # Chapter 1 스토리 시작
 def chapter1(player):
-    print(f'Chapter 1: {player.name} escapes.')
+    story_path = ''
+    for i in range(1):
+        choice = input('번호 선택 :  ')
+        if choice not in ['3', '2', '1']:
+            print('Invalid choice. Please enter 3, 2, or 1.')
+            continue
+        else:
+            if choice == '3' :
+                with open('story.txt', 'r', encoding='utf-8') as file:
+                    print(file.read())
+                player.points = 3
+                print(player.points)
+            elif choice == '2' :
+                with open('story.txt', 'r', encoding='utf-8') as file:
+                    print(file.read())
+                player.points = 3
+                print(player.points)
+            else:
+                print('Success!')
+                player.points += int(choice)
+                print(player.points)
+
+    if player.points == 3:
+        chapter1_sub(player)
+    elif player.points == 2:
+        chapter1_sub(player)
+    else:
+        chapter1_sub(player)
+
+
+
+def chapter1_sub(player):
+    # 스토리 폴더에 있는 모든 텍스트 파일을 나열합니다
+    available_stories = os.listdir('chapter1_sub_stories')
+    
     for i in range(4):
+        # 무작위로 스토리 파일을 선택하고 목록에서 제거합니다
+        story_file = random.choice(available_stories)
+        available_stories.remove(story_file)
+        
+        # 스토리를 읽고 출력합니다
+        with open(f'chapter1_sub_stories/{story_file}', 'r', encoding='utf-8') as file:
+            print(file.read())
+        
         choice = input('Choice (3, 2, or 1 points): ')
         if choice not in ['3', '2', '1']:
             print('Invalid choice. Please enter 3, 2, or 1.')
@@ -14,13 +66,21 @@ def chapter1(player):
             success_probability = 70 if choice == '3' else 80 if choice == '2' else 90
             if random.randint(1, 100) > success_probability:
                 print('Fail.')
-                player.points = 0
+                player.points = max(player.points - 1, 0)  # 가능하면 점수를 하나 뺍니다
+                print(player.points)
             else:
                 print('Success!')
+                print(player.points)
+
+        if not available_stories:
+            print(player.points)
+            print('실패하였습니다.')
+            break
+
     if player.points >= 10:
         chapter2(player)
     else:
-        print('Game over.')
+        print('실패하였습니다.')
 
 # Chapter 2 스토리 시작
 def chapter2(player):
@@ -38,10 +98,6 @@ def chapter2(player):
                 player.points -= 1
             else:
                 print('Success!')
-    if player.points >= 11:
-        chapter3(player)
-    else:
-        print('Game over.')
 
 # Chapter 3 스토리 시작
 def chapter3(player):
