99802a6a4de0722f85c61ee44cd0beccc617a5e0
정다성 <wjdektjd0878@gmail.com>
2023-06-26 18:43:19 +0900

    first commit

---
+++ backend/google_login/google_login.py
@@ -0,0 +1,72 @@
+# 필요한 패키지들을 가져옵니다.
+from flask import Flask, request, redirect, session
+from oauthlib.oauth2 import WebApplicationClient
+import os
+import requests
+
+# 애플리케이션 설정
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY", "비밀 키") # 실제로는 보안을 위해 랜덤한 문자열을 사용해야 합니다.
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+@app.route("/login")
+def login():
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+@app.route("/login/callback")
+def callback():
+    code = request.args.get("code")
+
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "사용자 이메일이 확인되지 않았습니다.", 400
+
+    session["user"] = unique_id
+    return "로그인에 성공했습니다."
+
+# 앱을 실행시킵니다.
+if __name__ == "__main__":
+    app.run(ssl_context="adhoc")
