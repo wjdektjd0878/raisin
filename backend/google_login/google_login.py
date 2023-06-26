780bcb6cd9c388888e1435c1c46cf7c674b4db99
정다성 <wjdektjd0878@gmail.com>
2023-06-26 18:57:06 +0900

    python 수정

--- backend/google_login/google_login.py
+++ backend/google_login/google_login.py
@@ -1,72 +1,30 @@
-# 필요한 패키지들을 가져옵니다.
-from flask import Flask, request, redirect, session
-from oauthlib.oauth2 import WebApplicationClient
-import os
-import requests
-
-# 애플리케이션 설정
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
+from flask import Flask, request, session
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "비밀 키") # 실제로는 보안을 위해 랜덤한 문자열을 사용해야 합니다.
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
+app.secret_key = 'your-secret-key'  # 실제 애플리케이션에서는 안전한 키를 사용해야 합니다.
 
-def get_google_provider_cfg():
-    return requests.get(GOOGLE_DISCOVERY_URL).json()
+GOOGLE_CLIENT_ID = "your-google-client-id"  # Google API Console에서 받은 클라이언트 ID를 입력해주세요.
 
-@app.route("/login")
-def login():
-    google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+@app.route("/login_with_token", methods=["POST"])
+def login_with_token():
+    try:
+        token = request.form.get("id_token")
 
-    request_uri = client.prepare_request_uri(
-        authorization_endpoint,
-        redirect_uri=request.base_url + "/callback",
-        scope=["openid", "email", "profile"],
-    )
-    return redirect(request_uri)
+        # Google의 공개 키로 ID 토큰을 검증합니다.
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
 
-@app.route("/login/callback")
-def callback():
-    code = request.args.get("code")
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            return "잘못된 발행자", 401
 
-    google_provider_cfg = get_google_provider_cfg()
-    token_endpoint = google_provider_cfg["token_endpoint"]
+        if idinfo['aud'] != GOOGLE_CLIENT_ID:
+            return "잘못된 클라이언트 ID", 401
 
-    token_url, headers, body = client.prepare_token_request(
-        token_endpoint,
-        authorization_response=request.url,
-        redirect_url=request.base_url,
-        code=code
-    )
-    token_response = requests.post(
-        token_url,
-        headers=headers,
-        data=body,
-        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
-    )
+        # ID 토큰이 제대로 검증되면, 유저의 Google ID를 세션에 저장합니다.
+        session["user"] = idinfo['sub']
+        return "로그인에 성공했습니다."
 
-    client.parse_request_body_response(json.dumps(token_response.json()))
-
-    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-    uri, headers, body = client.add_token(userinfo_endpoint)
-    userinfo_response = requests.get(uri, headers=headers, data=body)
-
-    if userinfo_response.json().get("email_verified"):
-        unique_id = userinfo_response.json()["sub"]
-        users_email = userinfo_response.json()["email"]
-        picture = userinfo_response.json()["picture"]
-        users_name = userinfo_response.json()["given_name"]
-    else:
-        return "사용자 이메일이 확인되지 않았습니다.", 400
-
-    session["user"] = unique_id
-    return "로그인에 성공했습니다."
-
-# 앱을 실행시킵니다.
-if __name__ == "__main__":
-    app.run(ssl_context="adhoc")
+    except ValueError:
+        # 토큰이 유효하지 않거나 만료되었을 경우 ValueError를 발생시킵니다.
+        return "토큰 검증에 실패했습니다.", 401
